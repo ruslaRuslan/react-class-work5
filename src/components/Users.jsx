58e0636b0857088eb9fc7cebf59843e60c8b0a3b
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-03 13:31:07 +0400

    komponent yarananda bu isi gorecek

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,20 +1,19 @@
 import axios from "axios";
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 const url = 'https://jsonplaceholder.typicode.com/users'
 
 
 const Users = () => {
-    const [users, setUsers] = useState([])
-    // destracche
-    axios.get(url)
-    // component ne zaman render edir?
-    // Render -> komponentin kodlarinin yeniden oxunub, UI-ya gorsenmesi. 
-    // komponent iki halda render edir!
-    // 1. State deyisilende 
-    // 2. Props deyisilende
-        .then(({ data }) => {
-            console.log(data);
-        })
+    const [users, setUsers] = useState([]) //komponent yarananda render etsin!
+
+    useEffect(() => {
+        axios.get(url)
+        .then(({ data }) => { // destracche
+                setUsers(data);
+            })
+    }, []) // bos list -> komponent yarananda bu isi gorecek
+
+
 
     return (
         // Fragment -> (adlanir yani bos teq)
@@ -33,4 +32,9 @@ const Users = () => {
     )
 }
 
+            // component ne zaman render edir?
+            // Render -> komponentin kodlarinin yeniden oxunub, UI-ya gorsenmesi. 
+            // komponent iki halda render edir!
+            // 1. State deyisilende 
+            // 2. Props deyisilende
 export default Users;

1258225a4aee3c94efe2499c95e51ed25a522c1d
ruslaRuslan <zeynalovruslan551@gmail.com>
2024-04-03 13:10:05 +0400

    ekrana adlar cixartdix

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import { useState } from 'react'
 
 const Counter = () => {
-  return (
-    <div>Counter</div>
-  )
+    return (
+        <div>Counter</div>
+    )
 }
 
 export default Counter
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,18 +1,33 @@
 import axios from "axios";
+import { useState } from 'react'
 const url = 'https://jsonplaceholder.typicode.com/users'
 
+
 const Users = () => {
+    const [users, setUsers] = useState([])
     // destracche
     axios.get(url)
     // component ne zaman render edir?
+    // Render -> komponentin kodlarinin yeniden oxunub, UI-ya gorsenmesi. 
+    // komponent iki halda render edir!
+    // 1. State deyisilende 
+    // 2. Props deyisilende
         .then(({ data }) => {
             console.log(data);
         })
+
     return (
         // Fragment -> (adlanir yani bos teq)
         <>
 
-
+            {
+                [
+                    <p>Ali</p>,
+                    <p>Ruslan</p>,
+                    <p>Sada</p>,
+                    <p>Sabina</p>,
+                ]
+            }
 
         </>
     )
